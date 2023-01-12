37cf7225f0334dcd6a591cd0b2e9935de000dba8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-01-12 14:17:39 +0100

    add few basic styles and svg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="container">
+    <h1>Weather App</h1>
     <search-input></search-input>
+    <div class="">
+      <img src="https://via.placeholder.com/400x300" class="time card-img-top" alt="" />
+      <div class="icon bg-light mx-auto text-center">
+        <!-- icon -->
+        <img src="" alt="" />
+      </div>
+      <div class="details">
+        <h5 class="">City name</h5>
+        <div class="">Weather condition</div>
+        <div class="">
+          <span>temp</span>
+          <span>&deg;C</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,6 +32,11 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
@@ -27,5 +48,18 @@ export default {
   padding-left: 15px;
   margin-right: auto;
   margin-left: auto;
+  text-align: center;
+}
+h1 {
+  color: #6c757d;
+  margin: 25px auto;
+}
+.card-img-top {
+  max-width: 100%;
+}
+.details {
+  text-align: center;
+  text-transform: uppercase;
+  color: #6c757d;
 }
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="change-location">
     <label for="city">Enter a location for weather information</label>
-    <input type="text" name="city" id="city" class="" />
+    <input type="text" name="city" id="city" class="input-search" />
   </form>
 </template>
 
@@ -9,4 +9,27 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+.change-location {
+  margin: 25px auto;
+}
+label {
+  display: inline-block;
+  margin-bottom: 0.5rem;
+}
+.input-search {
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+</style>
